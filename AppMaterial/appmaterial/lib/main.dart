c512e39979f0f394be4c63b6d3c3a48180ac3fd6
siblapin <sib.lapin@mail.ru>
2022-09-14 21:12:23 +0700

    Lessons_5.1

--- AppMaterial/appmaterial/lib/main.dart
+++ AppMaterial/appmaterial/lib/main.dart
@@ -5,7 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  final tabs = const [
+    Center(child: Text('1 страница')),
+    Center(child: Text('1 страница')),
+    Center(child: Text('1 страница'))
+  ];
 
   // This widget is the root of your application.
   @override
@@ -13,12 +21,57 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         drawer: const App_Drawer(),
+        endDrawer: App_EndDrawer(),
         appBar: AppBar(
           title: const Text('Flutter Demo Home Page'),
+          actions: [
+            Builder(
+              builder: (context) => IconButton(
+                  icon: Icon(Icons.account_circle),
+                  onPressed: (() {
+                    Scaffold.of(context).openEndDrawer();
+                  })),
+            )
+          ],
         ),
         bottomNavigationBar: const App_BottomNavigationBar(),
-        body: tabs[_currentIndex],
+        floatingActionButton: const App_FloatingActionButton(),
+        body: Container(child: Text('Страница')),
       ),
+    );
+  }
+}
+
+//showModalBottomSheet
+class App_FloatingActionButton extends StatelessWidget {
+  const App_FloatingActionButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+            context: context,
+            builder: ((context) => Container(
+                height: 200,
+                child: Column(children: [
+                  const Center(
+                      child: ListTile(
+                    leading: Icon(Icons.credit_card_outlined),
+                    title: Text('сумма',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    trailing: Text('200',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                  )),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text('Оплатить'))
+                ]))));
+      },
+      child: const Icon(Icons.add_outlined, size: 40),
     );
   }
 }
@@ -31,7 +84,54 @@ class App_Drawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer();
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+              child: Column(
+            children: const [
+              CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('image/avatar/foto1.jpg')),
+              Text('Avatar')
+            ],
+          )),
+          const ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Foto'),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          const ListTile(
+            leading: Icon(Icons.message),
+            title: Text('Chat'),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          const ListTile(
+            leading: Icon(Icons.album),
+            title: Text('Almums'),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          const SizedBox(height: 300),
+          SizedBox(
+              height: 50,
+              width: 250,
+              //color: Colors.green,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Выход'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Регистрация'),
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
   }
 }
 
@@ -48,12 +148,6 @@ class App_BottomNavigationBar extends StatefulWidget {
 
 class _App_BottomNavigationBarState extends State<App_BottomNavigationBar> {
   int _currentIndex = 0;
-  final tabs = const [
-    Center(child: Text('1 страница')),
-    Center(child: Text('1 страница')),
-    Center(child: Text('1 страница'))
-  ];
-
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
@@ -69,6 +163,32 @@ class _App_BottomNavigationBarState extends State<App_BottomNavigationBar> {
             _currentIndex = index;
           });
         },
+      ),
+    );
+  }
+}
+
+// EndDrawer
+class App_EndDrawer extends StatelessWidget {
+  const App_EndDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+              child: Column(
+            children: const [
+              CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('image/avatar/foto1.jpg')),
+              Text('Avatar')
+            ],
+          )),
+        ],
       ),
     );
   }
